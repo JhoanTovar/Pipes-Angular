50dcc02b48177ab4ede12aa17a98235f9af2ce64
Jhoan Tovar <tovarjhoan82sf@gmail.com>
2023-09-04 22:41:09 -0500

    Se agrega el proyecto de pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pipes';
+  fecha = new Date();
+  moneda = '5500';
+  cadena ='Hola mundo';
+  
 }
